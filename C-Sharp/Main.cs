184894b22ac401f0d131b84d6ad2e8f5ccd5bfd5
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2021-12-17 11:35:37 -0500

    Update Main.cs

--- C-Sharp/Main.cs
+++ C-Sharp/Main.cs
@@ -3,6 +3,7 @@ using System;
 namespace Hello {
     class Program {
         static void Main(string[] args) {
+            // division by zero
             int y = 0/0;
             Console.WriteLine(y);
         }
