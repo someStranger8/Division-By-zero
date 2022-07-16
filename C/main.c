9f318b3c9e28ad5fd6ff15d724cccaf6589d8674
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-07-16 18:05:21 -0400

    Update main.c

--- C/main.c
+++ C/main.c
@@ -1,9 +1,34 @@
+
+/*
+  division by zero
+*/
+
 #include<stdio>
 #include<stdiolib>
 
+void div(int a, int b) {
+  // count how many times b is in a
+  int i = 0;
+
+  // loop through it
+  while (a > b) {
+    // without remainder
+    if (a == b) {
+      int l = i + 1;
+      return "" + l;
+    }
+    
+    // keep counting
+    int i = i + 1;
+    int a = a - b;
+  }
+
+  // with remainder
+  return "" + i + " with a remainder of " + a;
+}
+
 int main() {
-    // division by zero
-    int y = 0/0;
-    printf y;
-    return 0;
+  // division by zero
+  printf(div(0,0));
+  return 0;
 }
