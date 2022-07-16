01ecdfb2417711e319a57d4ae2458f6eaa923324
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-07-16 18:39:52 -0400

    Update main.java

--- Java/main.java
+++ Java/main.java
@@ -1,9 +1,34 @@
+
+/*
+  division by zero
+*/
+
 import java.util.Math;
 
 public class Main {
     public static void Main(String[] args) {
         // division by zero
-        int y = 0/0;
-        System.out.printl(y);
+        System.out.println(div(0,0));
+    }
+
+    public static void div(int a, int b) {
+      // count how many times b is in a
+      int i = 0;
+
+      // loop through it
+      while (a >= b) {
+        // without remainder
+        if a == b {
+          var l = i + 1;
+          return l;
+        }
+
+        // keep counting
+        var i = i + 1;
+        var a = a - b;
+      }
+
+      // with remainder
+      return "" + i + " with a remainder of " + a;
     }
 }
