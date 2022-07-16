213914b3c1e0c8915733aa162f4f194dc5033a6e
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-07-16 17:47:22 -0400

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,33 +1,24 @@
 
-/*
+"""
   division by zero
-*/
+"""
 
-#include <iostream>
-using namespace std;
+def div(a, b):
+  # count how many times b is in a
+  i = 0
 
-char div(int a, int b) {
-  // count how many times b is in a
-  int i = 0;
-
-  // loop through it
-  while (a > b) {
-    // without remainder
-    if (a == 0) {
-      return "" + i;
-    }
+  # loop through it
+  while a > b:
+    # without remainder
+    if a == 0:
+      return i
     
-    // keep counting
-    int i = i + 1;
-    int a = a - b;
-  }
+    # keep counting
+    i+=1
+    int a-=b
 
-  // with remainder
-  return "" + i + " with a remainder of " + a;
+  # with remainder
+  return f"{i} with a remainder of {a}"
 }
 
-int main() {
-  // division by zero
-  cout << div(0,0) << endl;
-  return 0;
-}
+print(div(0,0))
