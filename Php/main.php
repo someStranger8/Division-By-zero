87ea401219dac530c99b6fbd9ff6e055c3e8b203
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-07-16 18:58:26 -0400

    Update main.php

--- Php/main.php
+++ Php/main.php
@@ -1,5 +1,28 @@
 <?php
-// division by zero
-$y = 0/0;
-echo y;
+
+/*
+  division by zero
+*/
+
+function div($a, $b) {
+  // count how many times a is in b
+  $i = 0;
+  
+  // loop through it
+  while ($a >= $b) {
+    if ($a == $b) {
+      // without remainder
+      return $i + 1;
+    }
+    
+    // keep counting
+    $i = $i + 1;
+    $a = $a - $b;
+  }
+  
+  // with remainder
+  return $i + " with a remainder of " + $a;
+}
+
+echo div(0,0);
 ?>
