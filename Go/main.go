92fdb5cf17834d3ae3870729dfa567825acf83da
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-07-16 18:32:51 -0400

    Update main.go

--- Go/main.go
+++ Go/main.go
@@ -1,5 +1,31 @@
+
+/*
+  division by zero
+*/
+
 package main
 import ("fmt")
+
+func div(a, b) {
+  // count how many times b is in a
+  var i = 0;
+
+  // loop through it
+  for a > b {
+    // without remainder
+    if a == b {
+      var l = i + 1;
+      return l;
+    }
+    
+    // keep counting
+    var i = i + 1;
+    var a = a - b;
+  }
+
+  // with remainder
+  return "" + i + " with a remainder of " + a;
+}
 
 func main() {
   // division by zero
