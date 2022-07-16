f036c82535acd68bcdeb0a0c480c514cc3d2d13f
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-07-16 18:48:23 -0400

    Update main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,3 +1,26 @@
-// division by zero
-var y = 0/0;
-console.log(y);
+
+/*
+  division by zero
+*/
+
+function div(a, b) {
+  // count how many times a is in b
+  var i = 0;
+  
+  // loop through it
+  while (a >= b) {
+    if (a == b) {
+      // without remainder
+      return i + 1;
+    }
+    
+    // keep counting
+    i+=1;
+    a-=b;
+  }
+  
+  // with remainder
+  return i + " with a remainder of " + a;
+}
+
+console.log(div(0,0));
