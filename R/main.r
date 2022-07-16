c41e66b131fa1f692321d7aa8bb2b21d483c2b92
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-07-16 19:03:19 -0400

    Update main.r

--- R/main.r
+++ R/main.r
@@ -1,3 +1,24 @@
+
 # division by zero
-y <- 0/0
-print(y)
+
+div <- function(a, b) {
+  # count how many times b is in a
+  i <- 0
+
+  # loop through it
+  while (a >= b) {
+    # without remainder
+    if (a == b) {
+      return i + 1
+    }
+    
+    # keep counting
+    i <- i + 1
+    a <- a - b
+  }
+
+  # with remainder
+  return i + " with a remainder of " + a
+}
+
+print(div(0,0))
