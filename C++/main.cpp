ff02a7fb8dd771bbd172c3200367b94fc9a8b73f
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-07-16 17:44:16 -0400

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,9 +1,33 @@
+
+/*
+  division by zero
+*/
+
 #include <iostream>
 using namespace std;
 
+char div(int a, int b) {
+  // count how many times b is in a
+  int i = 0;
+
+  // loop through it
+  while (a > b) {
+    // without remainder
+    if (a == 0) {
+      return "" + i;
+    }
+    
+    // keep counting
+    int i = i + 1;
+    int a = a - b;
+  }
+
+  // with remainder
+  return "" + i + " with a remainder of " + a;
+}
+
 int main() {
   // division by zero
-  int y = 0/0;
-  cout << y;
+  cout << div(0,0) << endl;
   return 0;
 }
