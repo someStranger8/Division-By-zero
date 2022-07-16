0d88be061aea85a023575fd93dd3d3350ef4b619
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-07-16 18:53:26 -0400

    Update main.kt

--- Kotlin/main.kt
+++ Kotlin/main.kt
@@ -1,5 +1,30 @@
+
+/*
+  division by zero
+*/
+
+fun div(a: int, b: int) {
+  // count how many times b is in a
+  var i = 0
+
+  // loop through it
+  while (a >= b) {
+    // without remainder
+    if (a == b) {
+      var l = i + 1
+      return l
+    }
+    
+    // keep counting
+    var i = i + 1
+    var a = a - b
+  }
+
+  // with remainder
+  return i + " with a remainder of " + a
+}
+
 fun main() { 
   // division by zero
-  var y = 0/0
-  println(y) 
+  println(div(0,0)) 
 }
