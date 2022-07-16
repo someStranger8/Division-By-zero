1b13b7c21d9a3da356cbc3027ba4c6376ab15791
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-07-16 18:03:39 -0400

    Update Main.cs

--- C-Sharp/Main.cs
+++ C-Sharp/Main.cs
@@ -1,11 +1,34 @@
+
+/*
+  division by zero
+*/
+
 using System;
 
 namespace Hello {
     class Program {
         static void Main(string[] args) {
-            // division by zero
-            int y = 0/0;
-            Console.WriteLine(y);
+            Console.WriteLine(div(0,0));
+        }
+
+        static void div(int a, int b) {
+            // count how many times b is in a
+            int i = 0;
+
+            // loop through it
+            while (a > b) {
+                // without remainder
+                if (a == b) {
+                  return i + 1;
+                }
+                
+                // keep counting
+                a -= b;
+                i++;
+            }
+
+            // with remainder
+            return i + " with a remainder of " + a;
         }
     }
 }
