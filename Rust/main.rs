d56ed20aaae202c08ef780e9f9c5d327f50b4b08
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-07-17 11:44:09 -0400

    Update main.rs

--- Rust/main.rs
+++ Rust/main.rs
@@ -1,5 +1,32 @@
+
+/*
+  division by zero
+*/
+
+use std::fmt;
+
+fn div(mut a, b) -> char {
+  // count how many times b is in a
+  let mut i = 0;
+  
+  // loop through it
+  while (a >= b) {
+    // without remainder
+    if (a == b) {
+      let r = format("{}", i + 1);
+      r
+    }
+    
+    // keep counting
+    let mut i = i + 1;
+    let mut a = a - b;
+  }
+  
+  // with remainder
+  let l = format("{} with a remainder of {}", i, a);
+  l
+}
+
 fn main() {
-    // division by zero
-    let y = 0/0
-    println!(y);
+  println(div(0,0));
 }
